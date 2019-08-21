b0438112c79968b1c5775ce91436fd8182291d62
Saravanan <saravinfotech@gmail.com>
2019-08-21 22:39:01 +0100

    Base UI Version

--- app/src/main/java/com/learning/tipcalculator/model/ResturantCalculator.kt
+++ app/src/main/java/com/learning/tipcalculator/model/ResturantCalculator.kt
@@ -1,10 +1,15 @@
 package com.learning.tipcalculator.model
 
+import java.math.RoundingMode
+
 class ResturantCalculator {
 
 
     fun calculatTip(checkAmount: Double, tipPct: Int): TipCalculation {
-        val tipAmount = checkAmount * (tipPct.toDouble() / 100.00)
+        val tipAmount = (checkAmount * (tipPct.toDouble() / 100.00))
+            .toBigDecimal()
+            .setScale(2, RoundingMode.HALF_UP)
+            .toDouble()
         val grandTotal = checkAmount + tipAmount;
         return TipCalculation(
             checkAmount,
--- app/src/test/java/com/learning/tipcalculator/model/ResturantCalculatorTest.kt
+++ app/src/test/java/com/learning/tipcalculator/model/ResturantCalculatorTest.kt
@@ -14,15 +14,19 @@ class ResturantCalculatorTest {
 
     @Test
     fun testCalculateTip() {
-        val checkInput = 10.00
-        val tipPctInput = 25
-        val expectedTipResult = TipCalculation(
-            checkAmount = checkInput,
-            tipPct = tipPctInput,
-            tipAmount = 2.5,
-            grandTotal = 12.5
+
+        val baseTC = TipCalculation(checkAmount = 10.00)
+
+        val testVals = listOf(
+            baseTC.copy(tipPct = 25, tipAmount = 2.5, grandTotal = 12.50),
+            baseTC.copy(tipPct = 15, tipAmount = 1.5, grandTotal = 11.50),
+            baseTC.copy(tipPct = 18, tipAmount = 1.8, grandTotal = 11.80)
         )
 
-        Assert.assertEquals(expectedTipResult, calculator.calculatTip(checkInput, tipPctInput))
+
+        testVals.forEach {
+            it
+            Assert.assertEquals(it, calculator.calculatTip(it.checkAmount, it.tipPct));
+        }
     }
 }

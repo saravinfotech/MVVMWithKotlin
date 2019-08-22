8dc79c9dea4b7060cfc931760bfbbe9034172716
Saravanan <saravinfotech@gmail.com>
2019-08-23 00:15:04 +0100

    ViewModelTest Included

--- app/src/test/java/com/learning/tipcalculator/viewmodel/CalculatorViewModelTest.kt
+++ app/src/test/java/com/learning/tipcalculator/viewmodel/CalculatorViewModelTest.kt
@@ -1,3 +1,26 @@
 package com.learning.tipcalculator.viewmodel
 
-class CalculatorViewModelTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class CalculatorViewModelTest {
+    lateinit var calculatorViewModel: CalculatorViewModel
+
+    @Before
+    fun setUp() {
+        calculatorViewModel = CalculatorViewModel()
+    }
+
+    @Test
+    fun testTipCalculator() {
+        calculatorViewModel.inputCheckAmount = "10.00"
+        calculatorViewModel.inputTipPct = "15"
+
+        calculatorViewModel.tipCalculator()
+        Assert.assertEquals("10.00", calculatorViewModel.tipCalculator.checkAmount)
+        Assert.assertEquals("15", calculatorViewModel.tipCalculator.tipPct)
+        Assert.assertEquals("1.50", calculatorViewModel.tipCalculator.tipAmount)
+        Assert.assertEquals("11.50", calculatorViewModel.tipCalculator.grandTotal)
+    }
+}
